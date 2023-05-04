58cfd96eca8293bc05904270e1e1a7e384ab1cb4
harrylongia <work4longia@gmail.com>
2023-05-05 01:01:30 +0530

    env var setup

--- lib/constants.tsx
+++ lib/constants.tsx
@@ -1,2 +1,2 @@
-export const REACT_APP_SUPABASE_URL: string = import.meta.env.VITE_APP_SUPABASE_URL;
-export const REACT_APP_SUPABASE_KEY: string = import.meta.env.VITE_APP_SUPABASE_KEY;
+export const REACT_APP_SUPABASE_URL: string = import.meta.env.VITE_APP_SUPABASE_URL || process.env.VITE_APP_SUPABASE_URL;
+export const REACT_APP_SUPABASE_KEY: string = import.meta.env.VITE_APP_SUPABASE_KEY || process.env.VITE_APP_SUPABASE_URL;
